d37b19d4667ba44edbff4b76219f384657bf3c11
Pavel <rusov_pavel2002@mail.ru>
2026-02-22 14:45:52 +0300

    feat(yandex metrics): add yandex metrics

--- web-client/app/layout.tsx
+++ web-client/app/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from 'next';
+import Script from 'next/script';
 import { Geist, Geist_Mono } from 'next/font/google';
 import './globals.css';
 import Navbar from '@/components/navbar/Navbar';
@@ -25,7 +26,46 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <Script
+          id="yandex-metrika"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+              (function(m,e,t,r,i,k,a){
+                  m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+                  m[i].l=1*new Date();
+                  for (var j = 0; j < document.scripts.length; j++) {
+                      if (document.scripts[j].src === r) { return; }
+                  }
+                  k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)
+              })(window, document,'script','https://mc.yandex.ru/metrika/tag.js?id=106953773', 'ym');
+
+              ym(106953773, 'init', {
+                ssr:true,
+                webvisor:true,
+                clickmap:true,
+                ecommerce:"dataLayer",
+                referrer: document.referrer,
+                url: location.href,
+                accurateTrackBounce:true,
+                trackLinks:true
+              });
+            `,
+          }}
+        />
+      </head>
+
       <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
+        <noscript>
+          <div>
+            <img
+              src="https://mc.yandex.ru/watch/106953773"
+              style={{ position: 'absolute', left: '-9999px' }}
+              alt=""
+            />
+          </div>
+        </noscript>
         <Navbar />
         <main className="pt-16">{children}</main>
       </body>
