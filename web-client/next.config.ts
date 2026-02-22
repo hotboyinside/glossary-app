63bd0ddaa2176d31f0588cdded1c1ea47b9ddcca
Pavel <rusov_pavel2002@mail.ru>
2026-02-22 23:34:12 +0300

    feat(next config): set up next config with csp headers

--- web-client/next.config.ts
+++ web-client/next.config.ts
@@ -1,7 +1,33 @@
 import type { NextConfig } from 'next';
 
+const cspHeader = `
+    default-src 'self';
+    script-src 'self' 'unsafe-eval' 'unsafe-inline' https://mc.yandex.ru;
+    style-src 'self' 'unsafe-inline';
+    img-src 'self' blob: data: https://mc.yandex.ru;
+    font-src 'self' data:;
+    object-src 'none';
+    base-uri 'self';
+    form-action 'self';
+    frame-ancestors 'none';
+    upgrade-insecure-requests;
+    connect-src 'self' https://mc.yandex.ru;
+`;
+
 const nextConfig: NextConfig = {
-  /* config options here */
+  async headers() {
+    return [
+      {
+        source: '/(.*)',
+        headers: [
+          {
+            key: 'Content-Security-Policy',
+            value: cspHeader.replace(/\n/g, ''),
+          },
+        ],
+      },
+    ];
+  },
   output: 'standalone',
 };
 
