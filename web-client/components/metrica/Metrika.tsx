550360c217ff424abe4d1f89edb4bbbb916c0eb0
Pavel <rusov_pavel2002@mail.ru>
2026-02-23 00:28:03 +0300

    fix(yandex metrics): yandex's metric script version #9

--- web-client/components/metrica/Metrika.tsx
+++ web-client/components/metrica/Metrika.tsx
@@ -2,6 +2,7 @@
 
 import { useEffect } from 'react';
 import { usePathname, useSearchParams } from 'next/navigation';
+import Script from 'next/script';
 
 const COUNTER_ID = 106953773;
 
@@ -11,33 +12,32 @@ export default function Metrika() {
 
   useEffect(() => {
     if (typeof window !== 'undefined' && window.ym) {
-      window.ym(COUNTER_ID, 'hit', window.location.href);
+      const url = pathName + (searchParams.toString() ? '?' + searchParams.toString() : '');
+      window.ym(COUNTER_ID, 'hit', url);
     }
   }, [pathName, searchParams]);
 
   return (
     <>
-      <script
-        id="yandex-metrika"
-        dangerouslySetInnerHTML={{
-          __html: `
-            (function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
-            m[i].l=1*new Date();
-            for (var j = 0; j < document.scripts.length; j++) {if (document.scripts[j].src === r) { return; }}
-            k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
-            (window, document, "script", "https://mc.yandex.ru/metrika/tag.js?id=${COUNTER_ID}", "ym");
+      <Script id="yandex-metrika" strategy="afterInteractive">
+        {`
+          (function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+          m[i].l=1*new Date();
+          for (var j = 0; j < document.scripts.length; j++) {if (document.scripts[j].src === r) { return; }}
+          k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
+          (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
 
-            ym(${COUNTER_ID}, "init", {
-              defer: true,
-              clickmap:true,
-              trackLinks:true,
-              accurateTrackBounce:true,
-              webvisor:true,
-              ecommerce:"dataLayer"
-            });
-          `,
-        }}
-      />
+          ym(${COUNTER_ID}, "init", {
+            defer: true,
+            ssr: true,
+            clickmap:true,
+            trackLinks:true,
+            accurateTrackBounce:true,
+            webvisor:true,
+            ecommerce:"dataLayer"
+          });
+        `}
+      </Script>
       <noscript>
         <div>
           <img
